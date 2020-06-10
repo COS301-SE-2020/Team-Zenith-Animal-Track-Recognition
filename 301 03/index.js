87fc093891c1ae458d111f18fadddc491b5141a2
GreenLizzerd <31959787+GreenLizzerd@users.noreply.github.com>
2020-06-10 07:20:18 +0200

    update

--- 301 03/index.js
+++ 301 03/index.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
+
 
 const app = express();
 const prot =4000;
 
-//google db
-const admin = require('firebase-admin');
 
-let serviceAccount = require('./erpzat-9ba766b1d3b2.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-let db = admin.firestore();
-//google db
-
+const schema = require('./schema/schema');
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
     schema,
@@ -26,12 +16,4 @@ app.use('/graphql', graphqlHTTP({
 app.listen(prot, () => {
     console.log('now listening for requests on port '+prot);
 });
-db.collection('AnimalInfo').get()
-  .then((snapshot) => {
-    snapshot.forEach((doc) => {
-      console.log(doc.id, '=>', doc.data());
-    });
-  })
-  .catch((err) => {
-    console.log('Error getting documents', err);
-  });
+
