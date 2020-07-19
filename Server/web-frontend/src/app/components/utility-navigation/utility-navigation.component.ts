aa1744cf866a0732d28ecf372027edac294d70f0
ZachGC <zachary.christophers@gmail.com>
2020-07-20 00:48:11 +0200

    Login Is fully functional
    I'll stop crying now

--- Server/web-frontend/src/app/components/utility-navigation/utility-navigation.component.ts
+++ Server/web-frontend/src/app/components/utility-navigation/utility-navigation.component.ts
@@ -1,4 +1,7 @@
+import { User } from './../../models/user';
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-utility-navigation',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./utility-navigation.component.css']
 })
 export class UtilityNavigationComponent implements OnInit {
+  currentUser: User;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {
+    this.authService.currentUser.subscribe(x => this.currentUser = x);
+  }
 
   ngOnInit(): void {
   }
 
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
