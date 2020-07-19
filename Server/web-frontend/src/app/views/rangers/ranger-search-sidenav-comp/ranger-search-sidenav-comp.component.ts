7b64c9ddad8d5d0d002df033796136e6c6b0a64e
ZachGC <zachary.christophers@gmail.com>
2020-07-19 16:40:08 +0200

    Init Commit
    Basic framework for ranger search functionality

--- Server/web-frontend/src/app/views/rangers/ranger-search-sidenav-comp/ranger-search-sidenav-comp.component.ts
+++ Server/web-frontend/src/app/views/rangers/ranger-search-sidenav-comp/ranger-search-sidenav-comp.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import {Ranger} from './../../../models/ranger';
-import {RANGERS} from './../../../models/mock-rangers';
+import { Ranger } from './../../../models/ranger';
+import { RANGERS } from './../../../models/mock-rangers';
 
 @Component({
-  selector: 'app-ranger-search-sidenav-comp',
-  templateUrl: './ranger-search-sidenav-comp.component.html',
-  styleUrls: ['./ranger-search-sidenav-comp.component.css']
+	selector: 'app-ranger-search-sidenav-comp',
+	templateUrl: './ranger-search-sidenav-comp.component.html',
+	styleUrls: ['./ranger-search-sidenav-comp.component.css']
 })
 export class RangerSearchSidenavCompComponent implements OnInit {
-	
+
 	//USING MOCK RANGER DATA. @Zach please replace this with an API call that fetches the users. We only need ID, Name, Username and ranger level
-	
+
 	rangers = RANGERS;
+	searchText;
 
 	constructor() { }
 
 	ngOnInit(): void {
-	  }
+	}
 
 }
