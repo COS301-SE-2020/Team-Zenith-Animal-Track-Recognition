58f326213039c9e292d41da64a74eb237c9d1811
ZachGC <zachary.christophers@gmail.com>
2020-07-19 20:09:30 +0200

    search and seperation
    Full functionality, requires CSS updates

--- Server/web-frontend/src/app/views/rangers/ranger-search-sidenav-comp/ranger-search-sidenav-comp.component.ts
+++ Server/web-frontend/src/app/views/rangers/ranger-search-sidenav-comp/ranger-search-sidenav-comp.component.ts
@@ -14,18 +14,50 @@ export class RangerSearchSidenavCompComponent implements OnInit {
 	rangers = RANGERS;
 	searchText;
 	currentAlphabet;
+	surnames: boolean = true;
+	levels: boolean = true;
 
 	constructor() { }
 
 	ngOnInit(): void {
 	}
 
-	checkIfNew(title: string) {
-		if (this.currentAlphabet === title.charAt(0).toLowerCase()) {
+	checkIfNew(title: string, pos: number) {
+		if (this.currentAlphabet === ("" + title).charAt(pos).toLowerCase()) {
 			return false;
 		} else {
-			this.currentAlphabet = title.charAt(0).toLowerCase();
+			this.currentAlphabet = ("" + title).charAt(pos).toLowerCase();
 			return true;
+		}
+	}
+
+	toggle(bool: boolean) {
+		this.surnames = bool;
+		this.levels = !bool;
+		this.sort(bool);
+	}
+
+	sort(bool: boolean) {
+		if (bool) {
+			for (let i = 0; i < this.rangers.length - 1; i++) {
+				for (let j = i + 1; j < this.rangers.length; j++) {
+					if (this.rangers[i].lastName > this.rangers[j].lastName) {
+						let temp = this.rangers[i];
+						this.rangers[i] = this.rangers[j];
+						this.rangers[j] = temp;
+					}
+				}
+			}
+		} else {
+			for (let i = 0; i < this.rangers.length - 1; i++) {
+				for (let j = i + 1; j < this.rangers.length; j++) {
+					if(this.rangers[i].rangerLevel > this.rangers[j].rangerLevel){
+						let temp = this.rangers[i];
+						this.rangers[i] = this.rangers[j];
+						this.rangers[j] = temp; 
+					}
+				}
+			}
 		}
 	}
 }
