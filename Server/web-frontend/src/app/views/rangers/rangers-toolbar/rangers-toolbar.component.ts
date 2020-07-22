cb8eb553bf9574e1c24a95f18577d898ac55d04c
ZachGC <zachary.christophers@gmail.com>
2020-07-22 11:22:06 +0200

    rebasing components

---
+++ Server/web-frontend/src/app/views/rangers/rangers-toolbar/rangers-toolbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rangers-toolbar',
+  templateUrl: './rangers-toolbar.component.html',
+  styleUrls: ['./rangers-toolbar.component.css']
+})
+export class RangersToolbarComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void 
+  {
+  }
+
+}
--- Server/web-frontend/src/app/views/rangers/rangers.component.ts
+++ Server/web-frontend/src/app/views/rangers/rangers.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Ranger } from './../../models/ranger';
-import { RANGERS } from './../../models/mock-rangers';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-rangers',
@@ -10,12 +9,7 @@ import { RANGERS } from './../../models/mock-rangers';
 export class RangersComponent implements OnInit {
 
   @ViewChild('sidenav') sidenav;
-  	rangers = RANGERS;
-	searchText;
-	currentAlphabet;
-	surnames: boolean = true;
-	levels: boolean = false;
-	
+  
   constructor() { }
 
   ngOnInit(): void {
@@ -35,42 +29,5 @@ export class RangersComponent implements OnInit {
 		document.getElementById("sidenav-open-btn-container").style.transitionDuration = "0.8s";
 		document.getElementById("sidenav-open-btn-container").style.left = "0%";
 	}
-	checkIfNew(title: string, pos: number) {
-		if (this.currentAlphabet === ("" + title).charAt(pos).toLowerCase()) {
-			return false;
-		} else {
-			this.currentAlphabet = ("" + title).charAt(pos).toLowerCase();
-			return true;
-		}
-	}
 
-	toggle(bool: boolean) {
-		this.surnames = bool;
-		this.levels = !bool;
-		this.sort(bool);
-	}
-
-	sort(bool: boolean) {
-		if (bool) {
-			for (let i = 0; i < this.rangers.length - 1; i++) {
-				for (let j = i + 1; j < this.rangers.length; j++) {
-					if (this.rangers[i].lastName.toUpperCase() > this.rangers[j].lastName.toUpperCase()) {
-						let temp = this.rangers[i];
-						this.rangers[i] = this.rangers[j];
-						this.rangers[j] = temp;
-					}
-				}
-			}
-		} else {
-			for (let i = 0; i < this.rangers.length - 1; i++) {
-				for (let j = i + 1; j < this.rangers.length; j++) {
-					if(this.rangers[i].rangerLevel > this.rangers[j].rangerLevel){
-						let temp = this.rangers[i];
-						this.rangers[i] = this.rangers[j];
-						this.rangers[j] = temp; 
-					}
-				}
-			}
-		}
-	}
 }
