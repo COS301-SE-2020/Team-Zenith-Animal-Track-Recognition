1045f898a5d5a07ff44e12edd0e547e47b1f06ad
Phahladira S Moja <60689831+Phahla@users.noreply.github.com>
2020-07-28 17:23:49 +0200

    Partly updated font and abstraction

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,23 +1,52 @@
 import 'package:ERP_RANGER/app/locator.dart';
-import 'package:ERP_RANGER/ui/widgets/bottom_navigation/bottom_nav.dart';
+import 'package:ERP_RANGER/services/theme.dart';
+import 'package:ERP_RANGER/services/util.dart';
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'services/api/graphQLConf.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'app/locator.dart';
 import 'app/router.gr.dart';
-void main() {
+
+GraphQLConfiguration graphQLConfiguration = GraphQLConfiguration();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   setupLocator();
   runApp(MyApp());
 }
+  
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  @override
+  _MyApp createState() => _MyApp();
+}
+
+class _MyApp extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: Routes.homeViewRoute,
-      onGenerateRoute: Router().onGenerateRoute,
-      navigatorKey: locator<NavigationService>().navigatorKey,
+   return FutureBuilder<bool>(
+      future: getLoggedIn(),
+      builder: (context, snapshot){
+        if(snapshot.hasError){
+          return progressIndicator();
+        }
+        if(snapshot.hasData) {
+          return snapshot.hasData == true ? MaterialApp(
+            theme: basicTheme(),
+            initialRoute: snapshot.data ? Routes.homeViewRoute : Routes.loginViewRoute,
+            onGenerateRoute: Router().onGenerateRoute,
+            navigatorKey: locator<NavigationService>().navigatorKey,
+          )
+          :progressIndicator();
+        }else{
+          return progressIndicator();
+        }
+      },
     );
   }
 }
+
 
