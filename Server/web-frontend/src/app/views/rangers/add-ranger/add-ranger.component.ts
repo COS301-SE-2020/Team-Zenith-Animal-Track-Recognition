1de56f33c5ae1c574be65c41c7ae711f9635e21a
ZachGC <zachary.christophers@gmail.com>
2020-07-29 09:55:06 +0200

    Minor bugfixes

--- Server/web-frontend/src/app/views/rangers/add-ranger/add-ranger.component.ts
+++ Server/web-frontend/src/app/views/rangers/add-ranger/add-ranger.component.ts
@@ -6,37 +6,35 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
-  selector: 'app-add-ranger',
-  templateUrl: './add-ranger.component.html',
-  styleUrls: ['./add-ranger.component.css']
+	selector: 'app-add-ranger',
+	templateUrl: './add-ranger.component.html',
+	styleUrls: ['./add-ranger.component.css']
 })
 export class AddRangerComponent implements OnInit {
-  addUserForm: FormGroup;
-  loading = false;
-  submitted = false;
+	addUserForm: FormGroup;
+	loading = false;
+	submitted = false;
 
-  hide = true;
-	constructor(private formBuilder: FormBuilder, private router: Router, private http: HttpClient, public dialogRef: MatDialogRef<AddRangerComponent>) {}
+	hide = true;
+	constructor(private formBuilder: FormBuilder, private router: Router, private http: HttpClient, public dialogRef: MatDialogRef<AddRangerComponent>) { }
 
-  ngOnInit(): void {
+	ngOnInit(): void {
 		this.addUserForm = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: ['', Validators.required],
-      phoneNumber: ['', Validators.required],
-      password: ['', Validators.required],
-      dob: ['', Validators.required]
-    });
-  }
+			firstName: ['', Validators.required],
+			lastName: ['', Validators.required],
+			email: ['', Validators.required],
+			phoneNumber: ['', Validators.required],
+			password: ['', Validators.required],
+			dob: ['', Validators.required]
+		});
+	}
 
-  get f() { return this.addUserForm.controls; }
+	get f() { return this.addUserForm.controls; }
 
-	onSubmit(test: boolean) 
-	{
-		if(test){return true;}
+	onSubmit(test: boolean) {
+		if (test) { return true; }
 		this.submitted = true;
-		if (this.addUserForm.invalid) 
-		{
+		if (this.addUserForm.invalid) {
 			return;
 		}
 		this.loading = true;
@@ -45,19 +43,17 @@ export class AddRangerComponent implements OnInit {
 		phoneNumber = phoneNumber.trim();
 
 		this.http.post<any>('http://putch.dyndns.org:55555/graphql?query=mutation{AddUser(firstName:"' + encodeURIComponent(this.f.firstName.value) + '",lastName:"' + encodeURIComponent(this.f.lastName.value) + '",Password:"' + encodeURIComponent(this.f.password.value) + '",Token:"' + JSON.parse(localStorage.getItem('currentToken'))['value'] + '",Access_Level:"1",e_mail:"' + encodeURIComponent(this.f.email.value) + '",phoneNumber:"' + encodeURIComponent(phoneNumber) + '"){lastName}}',
-		'mutation{AddUser(firstName:"' + encodeURIComponent(this.f.firstName.value) + '",lastName:"' + encodeURIComponent(this.f.lastName.value) + '",Password:"' + encodeURIComponent(this.f.password.value) + '",Token:"' + JSON.parse(localStorage.getItem('currentToken'))['value'] + '",Access_Level:"1",e_mail:"' + encodeURIComponent(this.f.email.value) + '",phoneNumber:"' + encodeURIComponent(phoneNumber) + '"){lastName}}')
-		.subscribe({next: data => this.dialogRef.close("success"), error: error => this.dialogRef.close("Error " + error.message)});
-	}	
-  
-    //Loader
-	startLoader()
-	{
+			'mutation{AddUser(firstName:"' + encodeURIComponent(this.f.firstName.value) + '",lastName:"' + encodeURIComponent(this.f.lastName.value) + '",Password:"' + encodeURIComponent(this.f.password.value) + '",Token:"' + JSON.parse(localStorage.getItem('currentToken'))['value'] + '",Access_Level:"1",e_mail:"' + encodeURIComponent(this.f.email.value) + '",phoneNumber:"' + encodeURIComponent(phoneNumber) + '"){lastName}}')
+			.subscribe({ next: data => this.dialogRef.close("success"), error: error => this.dialogRef.close("Error " + error.message) });
+	}
+
+	//Loader
+	startLoader() {
 		console.log("Starting Loader");
 		document.getElementById("loader-container").style.visibility = "visible";
-	}  
-	stopLoader()
-	{
-	  	console.log("Stopping Loader");
+	}
+	stopLoader() {
+		console.log("Stopping Loader");
 		document.getElementById("loader-container").style.visibility = "hidden";
 	}
 }
--- Server/web-frontend/src/app/views/rangers/ranger-search-sidenav-comp/ranger-search-sidenav-comp.component.ts
+++ Server/web-frontend/src/app/views/rangers/ranger-search-sidenav-comp/ranger-search-sidenav-comp.component.ts
@@ -1,79 +1,83 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Ranger } from './../../../models/ranger';
 import { RANGERS } from './../../../models/mock-rangers';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-	selector: 'app-ranger-search-sidenav-comp',
-	templateUrl: './ranger-search-sidenav-comp.component.html',
-	styleUrls: ['./ranger-search-sidenav-comp.component.css']
+  selector: 'app-ranger-search-sidenav-comp',
+  templateUrl: './ranger-search-sidenav-comp.component.html',
+  styleUrls: ['./ranger-search-sidenav-comp.component.css']
 })
 export class RangerSearchSidenavCompComponent implements OnInit {
-	rangers;
-	currentAlphabet;
-	surnames: boolean = true;
-	levels: boolean = false;
-	sorted: string;
-	searchText: string;
 
-	constructor(private http: HttpClient) { }
+  @Input() rangers;
+  @Input('rangerAutocompletePanel') classList: string;
+  @Output() rangersOnChange: EventEmitter<Object> = new EventEmitter();
 
-	ngOnInit(): void {
-		document.getElementById("rangers-route").classList.add("activeRoute");
-		this.http.get<any>('http://putch.dyndns.org:55555/graphql?query=query{Users(TokenIn:"' + JSON.parse(localStorage.getItem('currentToken'))['value'] +'"){Token,Password,Access_Level,e_mail,firstName,lastName}}')
-			.subscribe((data: any[]) => {
-				let temp = [];
-				temp = Object.values(Object.values(data)[0]);
-				this.printOut(temp);
-			});
-	}
+  currentAlphabet: any;
+  surnames: boolean = true;
+  levels: boolean = false;
+  sorted: string;
+  searchText: string;
 
-	printOut(temp: any) {
-		this.rangers = temp[0];
-		this.sort(true);
-	}
+  constructor(private http: HttpClient) { }
 
-	checkIfNew(title: string, pos: number) {
-		if (this.currentAlphabet === ("" + title).charAt(pos).toLowerCase()) {
-			return false;
-		} else {
-			this.currentAlphabet = ("" + title).charAt(pos).toLowerCase();
-			return true;
-		}
-	}
+  ngOnInit(): void {
+    document.getElementById("rangers-route").classList.add("activeRoute");
+    this.http.get<any>('http://putch.dyndns.org:55555/graphql?query=query{Users(TokenIn:"' + JSON.parse(localStorage.getItem('currentToken'))['value'] + '"){Token,Password,Access_Level,e_mail,firstName,lastName}}')
+      .subscribe((data: any[]) => {
+        let temp = [];
+        temp = Object.values(Object.values(data)[0]);
+        this.printOut(temp);
+      });
+  }
 
-	toggle(bool: boolean) {
-		this.surnames = bool;
-		this.levels = !bool;
-		this.sort(bool);
-	}
+  printOut(temp: any) {
+    this.rangers = temp[0];
+    this.sort(true);
+  }
 
-	sort(bool: boolean) {
-		let temp: string;
-		if (bool) {
-			for (let i = 0; i < this.rangers.length - 1; i++) {
-				for (let j = i + 1; j < this.rangers.length; j++) {
-					if (this.rangers[i].lastName.toUpperCase() > this.rangers[j].lastName.toUpperCase()) {
-						let temp = this.rangers[i];
-						this.rangers[i] = this.rangers[j];
-						this.rangers[j] = temp;
-					}
-				}
-			}
-			temp = "Sorted alphabetically";
-		} else {
-			for (let i = 0; i < this.rangers.length - 1; i++) {
-				for (let j = i + 1; j < this.rangers.length; j++) {
-					if (this.rangers[i].Access_Level > this.rangers[j].Access_Level) {
-						let temp = this.rangers[i];
-						this.rangers[i] = this.rangers[j];
-						this.rangers[j] = temp;
-					}
-				}
-			}
-			temp = "Sorted by ranger level";
-		}
-		this.sorted = temp;
-		return temp;
-	}
+  checkIfNew(title: string, pos: number) {
+    if (this.currentAlphabet === ("" + title).charAt(pos).toLowerCase()) {
+      return false;
+    } else {
+      this.currentAlphabet = ("" + title).charAt(pos).toLowerCase();
+      return true;
+    }
+  }
+
+  toggle(bool: boolean) {
+    this.surnames = bool;
+    this.levels = !bool;
+    this.sort(bool);
+  }
+
+  sort(bool: boolean) {
+    let temp: string;
+    if (bool) {
+      for (let i = 0; i < this.rangers.length - 1; i++) {
+        for (let j = i + 1; j < this.rangers.length; j++) {
+          if (this.rangers[i].lastName.toUpperCase() > this.rangers[j].lastName.toUpperCase()) {
+            let temp = this.rangers[i];
+            this.rangers[i] = this.rangers[j];
+            this.rangers[j] = temp;
+          }
+        }
+      }
+      temp = "Sorted alphabetically";
+    } else {
+      for (let i = 0; i < this.rangers.length - 1; i++) {
+        for (let j = i + 1; j < this.rangers.length; j++) {
+          if (this.rangers[i].Access_Level > this.rangers[j].Access_Level) {
+            let temp = this.rangers[i];
+            this.rangers[i] = this.rangers[j];
+            this.rangers[j] = temp;
+          }
+        }
+      }
+      temp = "Sorted by ranger level";
+    }
+    this.sorted = temp;
+    return temp;
+  }
 }
