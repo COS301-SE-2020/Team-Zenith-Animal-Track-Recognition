79ff99b18a24f3001907fe9b037eba861997acfb
Andrew Lumambo <u16091010@tuks.co.za>
2020-07-30 21:18:25 +0200

    Animal View animal list query string fix

--- Server/web-frontend/src/app/views/animals/animals.component.ts
+++ Server/web-frontend/src/app/views/animals/animals.component.ts
@@ -1,6 +1,6 @@
-import { ROOT_QUERY_STRING } from 'src/app/models/data';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ROOT_QUERY_STRING } from 'src/app/models/data';
 
 @Component({
 	selector: 'app-animals',
@@ -25,18 +25,13 @@ export class AnimalsComponent implements OnInit {
 		this.test = true;
 		document.getElementById("animals-route").classList.add("activeRoute");
 		this.http.get<any>(ROOT_QUERY_STRING + '?query=query{animals(Token:"' + JSON.parse(localStorage.getItem('currentToken'))['value'] +
-			'"){Classification,Animal_ID,Common_Name,Group_ID,HeightM,HeightF,WeightM,WeightF,Habitats,Diet_Type,Life_Span,Gestation_Period,Typical_Behaviour,' +
-			'Overview_of_the_animal,Description_of_animal,Pictures}}')
+			'"){Classification,Animal_ID,Common_Name,Group_ID{Group_Name},HeightM,HeightF,WeightM,WeightF,Habitats{Habitat_ID},Diet_Type,Life_Span,Gestation_Period,Typical_Behaviour,' +
+			'Overview_of_the_animal,Description_of_animal,Pictures{URL}}}')
 			.subscribe((data: any[]) => {
 				let temp = [];
 				temp = Object.values(Object.values(data)[0]);
 				this.animals = temp[0];
 			});
-	}
-
-	printOut(temp: any) {
-		this.animals = temp;
-		console.log(this.animals);
 	}
 
 	openSidenav() {
@@ -55,7 +50,9 @@ export class AnimalsComponent implements OnInit {
 	}
 
 	refresh() {
-		this.http.get<any>(ROOT_QUERY_STRING + 'query{Users(TokenIn:"' + JSON.parse(localStorage.getItem('currentToken'))['value'] + '"){Token,Password,Access_Level,e_mail,firstName,lastName,phoneNumber}}')
+		this.http.get<any>(ROOT_QUERY_STRING + '?query=query{animals(Token:"' + JSON.parse(localStorage.getItem('currentToken'))['value'] +
+			'"){Classification,Animal_ID,Common_Name,Group_ID{Group_Name},HeightM,HeightF,WeightM,WeightF,Habitats{Habitat_ID},Diet_Type,Life_Span,Gestation_Period,Typical_Behaviour,' +
+			'Overview_of_the_animal,Description_of_animal,Pictures{URL}}}')
 			.subscribe((data: any[]) => {
 				let temp = [];
 				temp = Object.values(Object.values(data)[0]);
