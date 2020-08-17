3b70dc90d3df4d8f2eb5587dceabf5cef77853c1
GreenLizzerd <31959787+GreenLizzerd@users.noreply.github.com>
2020-08-17 08:50:43 +0200

    billd app

---
+++ app/build/image_downloader/generated/source/buildConfig/debug/com/ko2ic/imagedownloader/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ko2ic.imagedownloader;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.ko2ic.imagedownloader";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.ko2ic.imagedownloader";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
